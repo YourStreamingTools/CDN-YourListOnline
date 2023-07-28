2a4a5d3fe8991583cecd2bebb6f010fc00a671e0
GFAUnDead <contact@gfaundead.stream>
2023-07-28 13:49:49 +1000

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,13 +1,15 @@
-$(document).ready(function() {
+(function($) {
+  $(document).ready(function() {
     $("#show-api-key").click(function() {
       $(".api-key-wrapper").show();
       $("#show-api-key").hide();
       $("#hide-api-key").show();
     });
-  
+
     $("#hide-api-key").click(function() {
       $(".api-key-wrapper").hide();
       $("#show-api-key").show();
       $("#hide-api-key").hide();
     });
-});
+  });
+})(jQuery);
