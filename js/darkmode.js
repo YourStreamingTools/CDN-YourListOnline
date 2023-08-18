15ded0a1cb17b5ea63d3bc825795c9ee1f12220c
gfaUnDead <contact@gfaundead.stream>
2023-08-18 22:14:01 +1000

    Update darkmode.js

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,6 +1,15 @@
 const darkModeToggle = document.getElementById('dark-mode-toggle');
 const body = document.body;
 
+window.addEventListener('load', () => {
+  const isDarkMode = localStorage.getItem('darkMode');
+  if (isDarkMode === 'true') {
+    body.classList.add('dark-mode');
+  }
+});
+
 darkModeToggle.addEventListener('click', () => {
-    body.classList.toogle('dark-mode');
-})
+  body.classList.toggle('dark-mode');
+  const isDarkMode = body.classList.contains('dark-mode');
+  localStorage.setItem('darkMode', isDarkMode);
+});
